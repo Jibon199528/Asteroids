510cd792ae0e989d4a70dee2575fe084351f92fd
Jibon199528 <stefanfoos11@gmail.com>
2025-03-03 19:52:02 +0100

    bug fix

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    
+    dt = 0
     
     while True:
         for event in pygame.event.get():
@@ -17,9 +19,10 @@ def main():
             
             
             
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for image in drawable:
+            image.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
